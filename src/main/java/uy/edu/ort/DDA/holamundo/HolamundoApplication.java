0793460fb12c7eb67d8479b3b106bf4367ea15fd
Mariana Betervide <mb153264@fi365.ort.edu.uy>
2026-09-06 16:47:35 -0300

    versión inicial

---
+++ src/main/java/uy/edu/ort/DDA/holamundo/HolamundoApplication.java
@@ -0,0 +1,30 @@
+package uy.edu.ort.DDA.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SpringBootApplication
+public class HolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoApplication.class, args);
+	}
+
+	@RestController 
+	public class HolaMundoController{
+
+		//decorar - mapear a la raíz del proyecto o carpeta x
+		//GET - verbo para acceder a url
+		//get, put, delete, post, patch - tengo otras acciones
+		@GetMapping("/holaMundo")
+		public String holaMundo(){
+			return "Hola Mundo";
+		}
+
+	}
+
+}
